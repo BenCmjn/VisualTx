cb10872d9fc8ff1883d4110d4ab139281ca4d449
BenCmjn <benjamin.calmejane@gmail.com>
2018-08-15 14:57:07 +0200

    address can be log in the console

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+var input;
+var submit;
+var output;
+// 0x4a071EEe72Bc8664C81B62836932eD0d246dA82b
+
 function setup() { 
+  input = select("#input");
+  submit= select("#submit");
+  
   createCanvas(400, 400);
+
+  input.changed(newText);
 } 
 
 function draw() { 
-  background(220);
+  background(155);
   rect(10,10,10,10);
+
+}
+
+function newText(){
+	console.log(input.value());
 }
