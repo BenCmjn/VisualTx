a16bc4aa729524e4bb6bdf5d01ecf3401c149362
BenCmjn <benjamin.calmejane@gmail.com>
2018-08-15 20:38:42 +0200

    clean array to draw figure with

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
 var input;
 var submit;
 var output;
-// 0x4a071EEe72Bc8664C81B62836932eD0d246dA82b
+
 
 function setup() {
-	noLoop();
+    noLoop();
     input = select("#input");
     submit = select("#submit");
 
     createCanvas(400, 400);
     input.changed(newText);
 
-    rect(10, 10, 10, 10);
+
 }
+
 function draw() {
-	
+
 }
 
 function newText() {
@@ -25,8 +26,29 @@ function newText() {
     var hash = address.slice(2, address.length);
     console.log("rest of the hash: " + hash);
 
-    var res = hash.split("");
-    console.log(res);
-    return res;
-    redraw();
+    var res = hash.split(""); 	// put each char of the hash in an array
+    var coord = unhex(res); 	// transform those char from HEX to NUMBERS
+    var numbersArr = Array.from(coord, x => x+=1); // add "1" to each of these NUMBERS
+
+    print("TRANSFORMED ARRAY: "+numbersArr); // expected output: Array without any 0
+    
+
+    var fig1 = numbersArr.slice(0, 10);
+
+    console.log("FIG.1: "+fig1);
+
+    triangle(fig1[0],fig1[1],fig1[2],fig1[3],fig1[4],fig1[5],fig1[6]);
+
+    // var fig1coord = 
+
+
+    // console.log(fig1coord[0]);
+    // console.log(res[0]);
+    // return res;
+    // redraw();
 }
+
+
+
+
+
