7e4874df18f92a4796564b806e54d9462fea688d
BenCmjn <benjamin.calmejane@gmail.com>
2018-08-15 21:18:59 +0200

    start style.css

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 var input;
 var submit;
 var output;
-
+var shapeCoef = 25; // maximumHEX = F(=16), 16*25 = 400 (canvasSize); 
+var colorCoef = 16; // maximumHEX = F(=16), 16*16 = 256 (max color); 
 
 function setup() {
     noLoop();
@@ -15,10 +16,12 @@ function setup() {
 }
 
 function draw() {
+	background(255);
 
 }
 
 function newText() {
+	redraw();
     var address = input.value();
     console.log("address entered: " + address);
     var type = address.slice(0, 2);
@@ -31,12 +34,13 @@ function newText() {
     var numbersArr = Array.from(coord, x => x+=1); // add "1" to each of these NUMBERS
 
     print("TRANSFORMED ARRAY: "+numbersArr); // expected output: Array without any 0
-    
+    var scaled = Array.from(numbersArr, x => x=x*shapeCoef); 	// draw it big
+    var colorReady = Array.from(numbersArr, x => x=x*colorCoef);// draw it bright
 
-    var fig1 = numbersArr.slice(0, 10);
+    var fig1 = scaled.slice(0, 6);
 
     console.log("FIG.1: "+fig1);
-
+    // fill();
     triangle(fig1[0],fig1[1],fig1[2],fig1[3],fig1[4],fig1[5],fig1[6]);
 
     // var fig1coord = 
@@ -45,7 +49,6 @@ function newText() {
     // console.log(fig1coord[0]);
     // console.log(res[0]);
     // return res;
-    // redraw();
 }
 
 
