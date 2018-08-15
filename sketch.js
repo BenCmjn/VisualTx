d76684bfb86914d3d33537ffb709d90b46677d20
BenCmjn <benjamin.calmejane@gmail.com>
2018-08-15 21:51:06 +0200

    first triangle is here

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var colorCoef = 16; // maximumHEX = F(=16), 16*16 = 256 (max color);
 
 function setup() {
     noLoop();
+    noStroke();
     input = select("#input");
     submit = select("#submit");
 
@@ -33,18 +34,18 @@ function newText() {
     var coord = unhex(res); 	// transform those char from HEX to NUMBERS
     var numbersArr = Array.from(coord, x => x+=1); // add "1" to each of these NUMBERS
 
-    print("TRANSFORMED ARRAY: "+numbersArr); // expected output: Array without any 0
+    print("TRANSFORMED ARRAY: "+numbersArr+"\nsize: "+numbersArr.length);; // expected output: Array without any 0
     var scaled = Array.from(numbersArr, x => x=x*shapeCoef); 	// draw it big
     var colorReady = Array.from(numbersArr, x => x=x*colorCoef);// draw it bright
 
-    var fig1 = scaled.slice(0, 6);
-
-    console.log("FIG.1: "+fig1);
-    // fill();
-    triangle(fig1[0],fig1[1],fig1[2],fig1[3],fig1[4],fig1[5],fig1[6]);
-
-    // var fig1coord = 
-
+    var fig1 = scaled.slice(0, 10);
+    // console.log("FIG.1: "+fig1+"\nsize: "+fig1.length);
+    var fig1Pos = scaled.slice(0, 6);
+    // console.log("Pos: "+fig1Pos+"\nsize: "+fig1Pos.length);
+    var fig1Col = colorReady.slice(6, 10);
+    // console.log("Col: "+fig1Col+"\nsize: "+fig1Col.length);
+    fill(fig1Col[0],fig1Col[1],fig1Col[2],fig1Col[3]);
+    triangle(fig1Pos[0],fig1Pos[1],fig1Pos[2],fig1Pos[3],fig1Pos[4],fig1Pos[5],fig1Pos[6]);
 
     // console.log(fig1coord[0]);
     // console.log(res[0]);
