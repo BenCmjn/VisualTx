a145f646088ca2fcd7f8cd3d39597dcfe10d012f
BenCmjn <benjamin.calmejane@gmail.com>
2018-08-15 15:24:48 +0200

    separate 0x from the rest of the hash, and split it in the array

--- sketch.js
+++ sketch.js
@@ -3,25 +3,26 @@ var submit;
 var output;
 // 0x4a071EEe72Bc8664C81B62836932eD0d246dA82b
 
-function setup() { 
-  input = select("#input");
-  submit= select("#submit");
+function setup() {
+    input = select("#input");
+    submit = select("#submit");
 
-  createCanvas(400, 400);
+    createCanvas(400, 400);
+    input.changed(newText);
 
-  input.changed(newText);
-} 
-
-function draw() { 
-  background(155);
-  rect(10,10,10,10);
-
+    background(155);
+    rect(10, 10, 10, 10);
 }
+//function draw() {}
 
-function newText(){
-	var str = input.value();
-	console.log(str);
+function newText() {
+    var address = input.value();
+    console.log("address entered: " + address);
+    var type = address.slice(0, 2);
+    console.log("type of address: " + type);
+    var hash = address.slice(2, address.length);
+    console.log("rest of the hash: " + hash);
 
-	var res = str.split("");
-	console.log(res);
+    var res = hash.split("");
+    console.log(res);
 }
